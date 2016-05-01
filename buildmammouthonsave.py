2f1b0691355ceb94f612bdb26e4ffbb0771c9133
Kostas Minaidis <kostas@creativecollective.gr>
2016-05-01 13:53:42 +0200

    Adding support for mmt extension and Windows -experimental- support

--- buildmammouthonsave.py
+++ buildmammouthonsave.py
@@ -1,7 +1,7 @@
 # REQUIREMENTS: npm install -g mammouth
-# USAGE: Files must have a .mammouth suffix
+# USAGE: Files must have a .mammouth or .mmt suffix
 
-import sublime, sublime_plugin,os
+import sublime, sublime_plugin,os,sys
 from os.path import dirname, realpath
 
 class BuildMammouthOnSave(sublime_plugin.EventListener):
@@ -9,9 +9,14 @@ class BuildMammouthOnSave(sublime_plugin.EventListener):
   def on_post_save(self, view):
     mammouthFile = view.file_name()
     filename, file_extension = os.path.splitext(mammouthFile)
-    if file_extension == ".mammouth":
+    if sublime.platform() == "windows" or sublime.platform() == "win32":
+      file_extension = file_extension.encode(sys.getfilesystemencoding())
+    if file_extension == ".mammouth" or file_extension == ".mmt":
       print("Compiling: " + mammouthFile)
-      view.window().run_command('exec',{'cmd': ["/usr/local/bin/mammouth", "-c", mammouthFile] })
+      if sublime.platform() == "windows" or sublime.platform() == "win32":
+        os.system("mammouth -c " + mammouthFile + " -o " + os.path.dirname(mammouthFile))
+      else:
+        view.window().run_command('exec',{'cmd': ["/usr/local/bin/mammouth", "-c", mammouthFile] })
 
 # REFERENCES
 # http://www.purplebeanie.com/Development/automatically-run-build-on-save-in-sublime-text-2.html
